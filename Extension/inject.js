2080dc4d866572ef9cd68a814ba8302b45e62ae9
Jonathan Alvarez <JonAlvarez22@gmail.com>
2022-08-14 05:42:43 -0600

    Update inject.js
    Updated URLs since the old ones were down.

--- Extension/inject.js
+++ Extension/inject.js
@@ -1,12 +1,12 @@
 function inject()
 {
     let aws = document.createElement("script");
-    aws.src = "https://cdn.global-gaming.co/resources/scripts/aws-polly-for-speechchat/Extension/aws-sdk.min.js";
+    aws.src = "https://github.com/JonathanAlvarez/AWS-Polly-for-SpeechChat/tree/master/Extension/aws-sdk.min.js";
     document.body.appendChild(aws);
     setTimeout(() => //Wait for AWS script to have initalised, it shouldnt take more than 5s
     {
         let tts = document.createElement("script");
-        tts.src = "https://cdn.global-gaming.co/resources/scripts/aws-polly-for-speechchat/Extension/tts.js";
+        tts.src = "https://github.com/JonathanAlvarez/AWS-Polly-for-SpeechChat/tree/master/Extension/tts.js";
         document.body.appendChild(tts);
     }, 5000);
 }
